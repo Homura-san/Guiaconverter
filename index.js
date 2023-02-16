a4e70e6b529926d03f6da22744324f2b840d736f
Homura-san <erickbispo@ufba.br>
2023-02-16 19:30:36 -0300

    modificar usuário

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const fs = require('fs')
 
-fs.readFile('./usuario.json', {encoding: 'utf-8'}, (err, dados) => {
-    if(err){
-        console.log("Um erro aconteceu.");
-    }else{
-        var conteudo = JSON.parse(dados); // Texto para objeto JS
-        conteudo.nome = "Hanako"
-        conteudo.curso = 'Imersão react'
-        conteudo.categoria = 'React'
+function modificarUsuario(nome, curso, categoria){
+    fs.readFile('./usuario.json', {encoding: 'utf-8'}, (err, dados) => {
+        if(err){
+            console.log("Um erro aconteceu.");
+        }else{
+            var conteudo = JSON.parse(dados); // Texto para objeto JS
+            conteudo.nome = nome
+            conteudo.curso = curso
+            conteudo.categoria = categoria
+    
+            fs.writeFile('./usuario.json', JSON.stringify(conteudo), (err) => {
+                if(err){
+                    console.log('Um erro aconteceu durante a escrita.');
+                }
+            })
+        }
+    });
+}
 
-        fs.writeFile('./usuario.json', JSON.stringify(conteudo), (err) => {
-            if(err){
-                console.log('Um erro aconteceu durante a escrita.');
-            }
-        })
-    }
-});
-
+modificarUsuario("Pedro Vitor", 'Formação Ruby', "Ruby")
