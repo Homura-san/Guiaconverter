ebcddda3c82a71bff4527a0e8362fadf77e532d1
Homura-san <erickbispo@ufba.br>
2023-02-18 15:25:44 -0300

    Leitor de arquivos

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
- 
+var Reader = require('./Reader')
+const fs = require('fs')
+
+var leitor = new Reader();
+
+
+
+async function main() {
+    var dados = await leitor.Read("./users.csv");
+    console.log(dados)
+}
+main();
