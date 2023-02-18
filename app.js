b1217f16d4f37b4bdc49e3c766c9863f9dd7dd4e
Homura-san <erickbispo@ufba.br>
2023-02-18 16:39:04 -0300

    Campo Virtual

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ async function main() {
     var dadosProcessados = Processor.Process(dados)
 
     var usuarios = new Table(dadosProcessados)
-    console.log(usuarios.rows);
+
+    console.log(usuarios.RowCount);
+    console.log(usuarios.ColumnCount);
 }
 main();
