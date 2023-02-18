fe8b6147d73c553cfa6a8de3705e148b74921713
Homura-san <erickbispo@ufba.br>
2023-02-18 16:28:02 -0300

    Table

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var Reader = require('./Reader')
-var Processor = require('./Processor')
+var Processor = require('./Processor')  
+var Table = require('./Table')
 
 const fs = require('fs')
 
@@ -10,5 +11,8 @@ var leitor = new Reader();
 async function main() {
     var dados = await leitor.Read("./users.csv");
     var dadosProcessados = Processor.Process(dados)
+
+    var usuarios = new Table(dadosProcessados)
+    console.log(usuarios.rows);
 }
 main();
